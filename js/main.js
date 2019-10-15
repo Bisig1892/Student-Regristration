95d602415aa423a6187dae57908dfde39f30b6de
TYLER Bisig <bisig3663@students.cptc.edu>
2019-10-15 14:05:57 -0700

    added displayStudent and showStudentData function need to hook up to html still

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.onload = main;
+var programAttr = 'data-program';
 function main() {
     var regBtn = document.querySelector('button');
     regBtn.onclick = processForm;
@@ -12,6 +13,24 @@ function processForm() {
     var newStudent = getStudentFromForm();
     displayStudent(newStudent);
     clearForm();
+}
+function displayStudent(s) {
+    var newItem = document.createElement('li');
+    newItem.innerText = s.firstName + '' + s.lastName;
+    var displaySelection = document.querySelector('#student-list');
+    var list = displaySelection.querySelector('ul');
+    newItem.setAttribute(programAttr, s.program);
+    newItem.setAttribute('data-address', s.address);
+    newItem.setAttribute('birthdate', s.dateOfBirth.toString());
+    newItem.onclick = showStudentData;
+    list.appendChild(newItem);
+}
+function showStudentData() {
+    var currListItem = this;
+    var name = currListItem.innerText;
+    var program = currListItem.getAttribute(programAttr);
+    document.querySelector('#display > h2').innerHTML = name;
+    document.querySelector('#display > p').innerHTML = name + ' is studying ' + program;
 }
 function getStudentFromForm() {
     var newStudent = new Student();
--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,7 @@
 window.onload = main;
 
+const programAttr = 'data-program';
+
 function main() {
     let regBtn = document.querySelector('button');
     regBtn.onclick = processForm;
@@ -28,6 +30,33 @@ function processForm() {
     let newStudent = getStudentFromForm();
     displayStudent(newStudent);
     clearForm();
+}
+
+function displayStudent(s:Student):void{ 
+    // Create <li>
+    let newItem = document.createElement('li');
+    newItem.innerText = s.firstName + '' + s.lastName;
+    let displaySelection = document.querySelector('#student-list');
+    let list = displaySelection.querySelector('ul');
+    // Embed student data in <li>
+    newItem.setAttribute(programAttr, s.program);
+    newItem.setAttribute('data-address', s.address);
+    newItem.setAttribute('birthdate', s.dateOfBirth.toString());
+    // Call showStudentData when <li> is clicked
+    newItem.onclick = showStudentData;
+    // Add <li> as a child to <ul>
+    list.appendChild(newItem);
+}
+
+/**
+ * Shows individual student data when a student <li> is clicked
+ */
+function showStudentData() {
+    let currListItem = <HTMLLIElement>this;
+    let name = currListItem.innerText;
+    let program = currListItem.getAttribute(programAttr);
+    document.querySelector('#display > h2').innerHTML = name;
+    document.querySelector('#display > p').innerHTML = name + ' is studying ' + program;
 }
 
 function getStudentFromForm():Student {
